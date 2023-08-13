f483a1b87a6964de6586b59ae0316fbbc6f936ba
h94usu6 <h94usu6@gmail.com>
2023-08-13 13:36:34 +0800

    update filename

--- lib/main_.dart
+++ lib/main_.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:ltrc/contants/routes.dart';
 import 'package:ltrc/extensions.dart';
 import 'package:ltrc/views/register_view.dart';
-import 'package:ltrc/views/teachWordView.dart';
-// import 'package:ltrc/views/units_view.dart';
+import 'package:ltrc/views/units_view.dart';
 import 'package:ltrc/views/words_view.dart';
 
 void main() {
@@ -13,6 +13,7 @@ void main() {
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -37,6 +38,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const TeachWordView();
+    return const UnitsView();
   }
 }
