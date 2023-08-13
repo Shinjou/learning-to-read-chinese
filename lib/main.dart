2605cb954f2c297ff6ba9c6df4c84f5b9a6dfe4a
h94usu6 <h94usu6@gmail.com>
2023-08-13 13:51:54 +0800

    edit filename and del empty files

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ltrc/contants/routes.dart';
 import 'package:ltrc/extensions.dart';
 import 'package:ltrc/views/register_view.dart';
-import 'package:ltrc/views/teachWordView.dart';
+import 'package:ltrc/views/teach_word_view.dart';
 // import 'package:ltrc/views/units_view.dart';
 import 'package:ltrc/views/words_view.dart';
 
