21a9c49f115c9e4361c8868f290543d4f25e325a
otter1021 <ginawang1021@gmail.com>
2023-08-19 23:02:03 +0800

    some minor changes

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ltrc/extensions.dart';
 import 'package:ltrc/widgets/mainPage/left_right_switch.dart';
-import 'package:ltrc/widgets/mainPage/circular container.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -37,114 +36,116 @@ class _RegisterViewState extends State<RegisterView> {
 
     return Scaffold(
       backgroundColor: '1E1E1E'.toColor(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-          height: deviceHeight * 0.35,
-          width: deviceWidth,
-          decoration: BoxDecoration(
-            color: '013E6D'.toColor(),
-          ),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: deviceHeight * 0.071),
-              Text(
-                  '年級',
-                  style: TextStyle(
-                      color: 'F5F5DC'.toColor(),
-                      fontSize: 44,
-                      fontFamily: 'Serif'
-                  )
-              ),
-              Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 23.0),
-                  child: Container(
-                      height: 9,
-                      width: 84,
-                      color: 'F5F5DC'.toColor()
-                  )
-              ),
-              Text(
-                  '課本版本',
-                  style: TextStyle(
-                      color: 'F5F5DC'.toColor(),
-                      fontSize: 44,
-                      fontFamily: 'Serif'
+      body: SizedBox.expand(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+            height: deviceHeight * 0.35,
+            width: deviceWidth,
+            decoration: BoxDecoration(
+              color: '013E6D'.toColor(),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: deviceHeight * 0.071),
+                Text(
+                    '年級',
+                    style: TextStyle(
+                        color: 'F5F5DC'.toColor(),
+                        fontSize: 44,
+                        fontFamily: 'Serif'
+                    )
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 23.0),
+                    child: Container(
+                        height: 9,
+                        width: 84,
+                        color: 'F5F5DC'.toColor()
+                    )
+                ),
+                Text(
+                    '課本版本',
+                    style: TextStyle(
+                        color: 'F5F5DC'.toColor(),
+                        fontSize: 44,
+                        fontFamily: 'Serif'
+                    )
+                )
+              ],
+              )
+            ),
+            SizedBox(height: deviceHeight * 0.057),
+            Icon(
+              Icons.home_filled,
+              color: 'F8A23A'.toColor(),
+              size: deviceWidth * 0.18,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: deviceHeight * 0.04),
+              child: LeftRightSwitch(
+                  iconsColor: 'F5F5DC'.toColor(),
+                  iconsSize: deviceWidth * 0.15,
+                  middleWidget: Container(
+                    alignment: AlignmentDirectional.center,
+                    width: 224,
+                    height: 57,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadiusDirectional.circular(20),
+                        color: '7DDEF8'.toColor()
+                    ),
+                    child: Text(
+                      '一年級',
+                      style: TextStyle(
+                        color: '000000'.toColor(),
+                        fontSize: 34.0,
+                        fontFamily: 'Serif'
+                      )
+                    )
                   )
               )
-            ],
-            )
-          ),
-          SizedBox(height: deviceHeight * 0.057),
-          Icon(
-            Icons.home_filled,
-            color: 'F8A23A'.toColor(),
-            size: deviceWidth * 0.18,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: deviceHeight * 0.04),
-            child: LeftRightSwitch(
+            ),
+            LeftRightSwitch(
                 iconsColor: 'F5F5DC'.toColor(),
                 iconsSize: deviceWidth * 0.15,
                 middleWidget: Container(
-                  alignment: AlignmentDirectional.center,
-                  width: 224,
-                  height: 57,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadiusDirectional.circular(20),
-                      color: '7DDEF8'.toColor()
-                  ),
-                  child: Text(
-                    '一年級',
-                    style: TextStyle(
-                      color: '000000'.toColor(),
-                      fontSize: 34.0,
-                      fontFamily: 'Serif'
+                    alignment: AlignmentDirectional.center,
+                    width: 224,
+                    height: 57,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadiusDirectional.circular(20),
+                        color: '7DDEF8'.toColor()
+                    ),
+                    child: Text(
+                        '翰林',
+                        style: TextStyle(
+                            color: '000000'.toColor(),
+                            fontSize: 34.0,
+                            fontFamily: 'Serif'
+                        )
                     )
-                  )
                 )
-            )
-          ),
-          LeftRightSwitch(
-              iconsColor: 'F5F5DC'.toColor(),
-              iconsSize: deviceWidth * 0.15,
-              middleWidget: Container(
-                  alignment: AlignmentDirectional.center,
-                  width: 224,
-                  height: 57,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadiusDirectional.circular(20),
-                      color: '7DDEF8'.toColor()
-                  ),
-                  child: Text(
-                      '翰林',
-                      style: TextStyle(
-                          color: '000000'.toColor(),
-                          fontSize: 34.0,
-                          fontFamily: 'Serif'
-                      )
-                  )
-              )
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, deviceHeight * 0.0687, 0, deviceHeight * 0.05),
-            child: Container(
-              alignment: AlignmentDirectional.center,
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                color: 'F8A23A'.toColor(),
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                Icons.chevron_right,
-                color: '1E1E1E'.toColor(),
-                size: 80
-              )
             ),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, deviceHeight * 0.0687, 0, deviceHeight * 0.05),
+              child: Container(
+                alignment: AlignmentDirectional.center,
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: 'F8A23A'.toColor(),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.chevron_right,
+                  color: '1E1E1E'.toColor(),
+                  size: 80
+                )
+              ),
+            )
+          ],
+        ),
       )
     );
   }
