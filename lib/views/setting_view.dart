a67b3005d25292c65bb57a7f85b654b85d36f494
otter1021 <ginawang1021@gmail.com>
2023-08-24 23:16:45 -0400

    complete setting view

--- lib/views/setting_view.dart
+++ lib/views/setting_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ltrc/extensions.dart';
+import 'package:ltrc/widgets/grade_and_provider_button.dart';
+import 'package:flutter/cupertino.dart';
 
 class SettingView extends StatefulWidget {
   const SettingView({super.key});
@@ -10,6 +12,8 @@ class SettingView extends StatefulWidget {
 }
 
 class _SettingViewState extends State<SettingView> {
+  bool switchValue = true;
+
   @override
   Widget build(BuildContext context) {
 
@@ -80,8 +84,7 @@ class _SettingViewState extends State<SettingView> {
                         color: '#F5F5DC'.toColor(),
                       ),
                       child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
+                          children: [
                             Container(
                               height: 55,
                               width: deviceWidth * 0.85,
@@ -107,12 +110,49 @@ class _SettingViewState extends State<SettingView> {
                                   ]
                               ),
                             ),
+                              Container(
+                                  height: 30,
+                                  width: deviceWidth * 0.882,
+                                  margin: EdgeInsetsDirectional.fromSTEB(deviceWidth * 0.064, 0, 0, 5),
+                                  child: Text(
+                                      '年級',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontFamily: 'Serif',
+                                          color: '#000000'.toColor()
+                                      )
+                                  )
+                              ),
+                            Container(
+                              height: deviceHeight * 0.0284,
+                              width: deviceWidth * 0.8,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '一年級'),
+                                  GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '二年級'),
+                                  GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '三年級')
+                                ]
+                              )),
+                              Container(
+                                height: deviceHeight * 0.0284,
+                                width: deviceWidth * 0.8,
+                                margin: EdgeInsetsDirectional.only(top: deviceHeight * 0.027),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '四年級'),
+                                    GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '五年級'),
+                                    GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '六年級')
+                                  ]
+                                )
+                              ),
                             Container(
                                 height: 30,
-                                width: 80,
-                                margin: EdgeInsetsDirectional.fromSTEB(deviceWidth * 0.064, 0, 0, 5),
+                                width: deviceWidth * 0.882,
+                                margin: EdgeInsetsDirectional.fromSTEB(deviceWidth * 0.064, deviceHeight * 0.039, 0, 5),
                                 child: Text(
-                                    '年級',
+                                    '課本版本',
                                     style: TextStyle(
                                         fontSize: 24,
                                         fontFamily: 'Serif',
@@ -120,10 +160,72 @@ class _SettingViewState extends State<SettingView> {
                                     )
                                 )
                             ),
-                            //年級按鍵
-                            //課本版本
-                            //課本版本按鍵
-                            //聲音 ＋ switch鍵
+                            Container(
+                                height: deviceHeight * 0.0284,
+                                width: deviceWidth * 0.8,
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '康軒'),
+                                      GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '翰林'),
+                                      GradeAndProviderButton(buttonWidth: deviceWidth * 0.24, buttonHeight: deviceHeight * 0.0284, text: '南一')
+                                    ]
+                                )
+                            ),
+                            Padding(
+                              padding: EdgeInsetsDirectional.symmetric(horizontal: 0, vertical: deviceHeight * 0.04),
+                              child: Container(
+                                height: 30,
+                                width: deviceWidth * 0.8,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      '聲音',
+                                      style: TextStyle(
+                                        color: '#000000'.toColor(),
+                                        fontSize: 24,
+                                        fontFamily: 'Serif'
+                                      )
+                                    ),
+                                    CupertinoSwitch(
+                                      onChanged: (bool? value){
+                                        setState(() {
+                                          switchValue = value ?? false;
+                                        });
+                                      },
+                                      value: switchValue,
+                                      activeColor: CupertinoColors.black,
+                                    )
+                                  ]
+                                )
+                              )
+                            ),
+                            Container(
+                                height: 30,
+                                width: deviceWidth * 0.8,
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Text(
+                                          '資源出處',
+                                          style: TextStyle(
+                                              color: '#000000'.toColor(),
+                                              fontSize: 24,
+                                              fontFamily: 'Serif'
+                                          )
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: Icon(Icons.arrow_forward_ios),
+                                        color: '#000000'.toColor(),
+                                        iconSize: 25
+                                      )
+                                    ]
+                                )
+                            )
+                            //資源出處
+                            //登出
                           ]
                       )
                   )
--- lib/widgets/grade_and_provider_button.dart
+++ lib/widgets/grade_and_provider_button.dart
@@ -4,17 +4,28 @@ import 'package:ltrc/extensions.dart';
 class GradeAndProviderButton extends StatelessWidget {
   final double buttonWidth;
   final double buttonHeight;
+  final String text;
   const GradeAndProviderButton(
       {Key? key,
         required this.buttonWidth,
-        required this.buttonHeight,})
+        required this.buttonHeight,
+        required this.text})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: buttonWidth,
       height: buttonHeight,
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 6),
+            blurRadius: 6
+          )
+        ]
+      ),
       child: ElevatedButton(
         onPressed: () {},
         style: ButtonStyle(
@@ -23,10 +34,16 @@ class GradeAndProviderButton extends StatelessWidget {
               borderRadius: BorderRadius.circular(20)
             ),
           ),
-          backgroundColor: "#D9D9D9".toColor(),
-
+          backgroundColor: MaterialStatePropertyAll('D9D9D9'.toColor())
         ),
-        child: Text()
+        child: Text(
+          '$text',
+          style: TextStyle(
+            color: '000000'.toColor(),
+            fontSize: 11,
+            fontFamily: 'Serif'
+          )
+        )
       ),
     );
   }
