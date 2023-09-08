da457afd9b37069f93bde0948e5ab3c6a6209e40
dlion168 <even.dlion8@gmail.com>
2023-09-08 21:12:28 +0800

    Add all.sqlite and modify attributes of words.

--- lib/data/providers/word_provider.dart
+++ lib/data/providers/word_provider.dart
@@ -10,11 +10,11 @@ import '../models/word_model.dart';
 class WordProvider {
   static Database? database;
   static Future<Database> getDBConnect() async {
-    String newPath = join(await getDatabasesPath(), 'words.sqlite');
+    String newPath = join(await getDatabasesPath(), 'all.sqlite');
     final exist = await databaseExists(newPath);
     if (!exist) {
       try {
-          ByteData data = await rootBundle.load(join("assets/data_files", "vocabulary.sqlite"));
+          ByteData data = await rootBundle.load(join("assets/data_files", "all.sqlite"));
           List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
           await File(newPath).writeAsBytes(bytes, flush: true);
       } 
@@ -27,20 +27,22 @@ class WordProvider {
     return database!;
   }
 
-  static String tableName = 'vocabulary_utf8';
+  static String tableName = 'Words';
   
   // Define constants for database
+  static const String databaseId = 'id';
   static const String databaseWord = 'word';
-  static const String databaseVocab1 = 'vocab_1';
-  static const String databaseMeaning1 = 'meaning_1';
-  static const String databaseSentence1 = 'sentence_1';
-  static const String databaseVocab2 = 'vocab_2';
-  static const String databaseMeaning2 = 'meaning_2';
-  static const String databaseSentence2 = 'sentence_2';
+  static const String databasePhoneticTone = 'phonetic_tone';
+  static const String databasePhonetic = 'phonetic';
+  static const String databaseTone = 'tone';
+  static const String databaseShapeSymbol = 'shape_symbol';
+  static const String databaseSoundSymbol = 'sound_symbol';
+  static const String databaseStrokes = 'strokes';
+  static const String databaseCommon = 'common';
 
   static Future<Database> initDatabase() async =>
     database ??= await openDatabase(
-      join(await getDatabasesPath(), 'words.sqlite'),
+      join(await getDatabasesPath(), 'all.sqlite'),
       version: 1,
     );
 
@@ -49,29 +51,57 @@ class WordProvider {
     await db.insert(
       tableName,
       word.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
+      conflictAlgorithm: ConflictAlgorithm.ignore,
     );
   }
-    
+  // static Future<void> addWord(Word word) async {
+  //   final Database db = await getDBConnect();
+  //   await db.insert(
+  //     tableName,
+  //     word.toMap(),
+  //     conflictAlgorithm: ConflictAlgorithm.replace,
+  //   );
+  // }
+
   static Future<Word> getWord({required String inputWord}) async {
     final Database db = await getDBConnect();
-    final List<Map<String, dynamic>> tmp = await db.rawQuery("PRAGMA table_info($tableName)");
-    debugPrint(tmp.toString());
     final List<Map<String, dynamic>> maps = await db.query(tableName,
-      columns: [databaseWord, databaseVocab1, databaseMeaning1, databaseSentence1, databaseVocab2, databaseMeaning2, databaseSentence2],
+      columns: ["*"],
       where: "$databaseWord=?",
       whereArgs: [inputWord]
     );
     return Word(
+      id: maps[0][databaseId]
       word: maps[0][databaseWord],
-      vocab1: maps[0][databaseVocab1],
-      meaning1: maps[0][databaseMeaning1],
-      sentence1: maps[0][databaseSentence1],
-      vocab2: maps[0][databaseVocab2],
-      meaning2: maps[0][databaseMeaning2],
-      sentence2: maps[0][databaseSentence2],
+      vocab1: maps[0][databasePhoneticTone],
+      meaning1: maps[0][databasePhonetic],
+      sentence1: maps[0][databaseTone],
+      vocab2: maps[0][databaseShapeSymbol],
+      meaning2: maps[0][databaseSoundSymbol],
+      sentence2: maps[0][databaseStrokes],
+      sentence2: maps[0][databaseCommon],
     );
   }
+    
+  // static Future<Word> getWord({required String inputWord}) async {
+  //   final Database db = await getDBConnect();
+  //   final List<Map<String, dynamic>> tmp = await db.rawQuery("PRAGMA table_info($tableName)");
+  //   debugPrint(tmp.toString());
+  //   final List<Map<String, dynamic>> maps = await db.query(tableName,
+  //     columns: [databaseWord, databaseVocab1, databaseMeaning1, databaseSentence1, databaseVocab2, databaseMeaning2, databaseSentence2],
+  //     where: "$databaseWord=?",
+  //     whereArgs: [inputWord]
+  //   );
+  //   return Word(
+  //     word: maps[0][databaseWord],
+  //     vocab1: maps[0][databaseVocab1],
+  //     meaning1: maps[0][databaseMeaning1],
+  //     sentence1: maps[0][databaseSentence1],
+  //     vocab2: maps[0][databaseVocab2],
+  //     meaning2: maps[0][databaseMeaning2],
+  //     sentence2: maps[0][databaseSentence2],
+  //   );
+  // }
 
   static void closeDb() async{
     await deleteDatabase(
