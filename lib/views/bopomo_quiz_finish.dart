14f831601045c6a88a6c1b2cb4bf0235ee3b5d18
Shinjou <shinjou.fang@gmail.com>
2024-02-01 16:26:01 +0800

    bpm UI changes

--- lib/views/bopomo_quiz_finish.dart
+++ lib/views/bopomo_quiz_finish.dart
@@ -1,73 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:ltrc/extensions.dart';
-import 'package:ltrc/views/view_utils.dart';
 
 class BopomoQuizFinishView extends StatelessWidget {
   const BopomoQuizFinishView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double fontSize =
-        getFontSize(context, 16); // 16 is the base font size for 360dp width
-
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.chevron_left, size: fontSize * 1.0),
-            onPressed: () => Navigator.pop(context),
-          ),
-          title: Text(
-            "拼拼看",
-            style: TextStyle(fontSize: fontSize * 1.0),
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(
-                Icons.home,
-                size: fontSize * 1.0,
+      appBar: AppBar(
+        leading: IconButton(icon: const Icon(Icons.chevron_left), onPressed: () => Navigator.pop(context),),
+        title: const Text("拼拼看"),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.home,
+            ),
+            onPressed: () => Navigator.of(context).pushNamed('/mainPage'),
+          )
+        ],
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center, 
+          children: [
+            Container(
+              height: 60,
+              width: 240,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: '#013E6D'.toColor(),
               ),
-              onPressed: () => Navigator.of(context).pushNamed('/mainPage'),
+              child: Text(
+                '⭐恭喜⭐\n完成所有題目！',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: "#F5F5DC".toColor(),
+                )
+              )
+            ),
+            const SizedBox(
+              height: 30
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pushNamed("/bopomoQuiz"), 
+              child: const Text(
+                "重新測驗",
+                style: TextStyle(color: Colors.black, fontSize: 16)
+              )
+            ),
+            const SizedBox(
+              height: 15
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pushNamed("/mainPage"), 
+              child: const Text(
+                "回首頁",
+                style: TextStyle(color: Colors.black, fontSize: 16)  
+              )
             )
           ],
-        ),
-        body: Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                    height: fontSize * 3.5,
-                    width: fontSize * 14.0,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: '#013E6D'.toColor(),
-                    ),
-                    child: Text('⭐恭喜⭐\n完成所有題目！',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: fontSize,
-                          color: "#F5F5DC".toColor(),
-                        ))),
-                SizedBox(
-                  height: fontSize * 0.3,
-                ),
-                ElevatedButton(
-                    onPressed: () =>
-                        Navigator.of(context).pushNamed("/bopomoQuiz"),
-                    child: Text("重新測驗",
-                        style: TextStyle(
-                            color: Colors.black, fontSize: fontSize))),
-                SizedBox(
-                  height: fontSize * 0.3,
-                ),
-                ElevatedButton(
-                    onPressed: () =>
-                        Navigator.of(context).pushNamed("/mainPage"),
-                    child: Text("回首頁",
-                        style:
-                            TextStyle(color: Colors.black, fontSize: fontSize)))
-              ],
-            )));
+        )
+      )
+    );
   }
 }
--- lib/views/bopomos_view.dart
+++ lib/views/bopomos_view.dart
@@ -2,94 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:ltrc/data/models/word_status_model.dart';
 import 'package:ltrc/extensions.dart';
 import '../widgets/word_card.dart';
-import '../views/view_utils.dart';
 
 class BopomosView extends StatelessWidget {
   const BopomosView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double fontSize =
-        getFontSize(context, 16); // 16 is the base font size for 360dp width
-
     dynamic obj = ModalRoute.of(context)!.settings.arguments;
-    List<WordStatus> wordsStatus = obj['wordStatus'];
-    List<Map> wordsPhrase = obj['wordsPhrase'];
+    List<WordStatus> wordsStatus = obj['wordStatus']; 
+    List<Map> wordsPhrase = obj['wordsPhrase']; 
 
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.chevron_left, size: fontSize * 1.0),
-            onPressed: () => Navigator.pop(context),
-          ),
-          title: Text(
-            "00 | 學注音",
-            style: TextStyle(
-                fontSize: fontSize * 1.0), // Set the font size for the title
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.home, size: fontSize * 1.0),
-              onPressed: () => Navigator.of(context).pushNamed('/mainPage'),
-            )
-          ],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.chevron_left),
+          onPressed: () => Navigator.pop(context),
         ),
-        body: CustomScrollView(
-          slivers: [
-            SliverPadding(
-              padding: const EdgeInsets.fromLTRB(14, 8, 14, 16),
-              sliver: SliverGrid(
-                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: isTablet(context)
-                      ? fontSize * 8.0
-                      : fontSize * 8.0, // This is most important
-                  mainAxisSpacing: fontSize * 0.5, // Reduced spacing
-                  crossAxisSpacing: fontSize * 0.5, // Reduced spacing
-                  childAspectRatio: isTablet(context) ? 4 / 3 : 4 / 4,
-                ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return WordCard(
-                      disable: false,
-                      isBpmf: true,
-                      unitId: 0,
-                      unitTitle: "學注音",
-                      wordsStatus: wordsStatus,
-                      wordsPhrase: wordsPhrase,
-                      wordIndex: index,
-                      sizedBoxWidth: fontSize *
-                          2.0, // 1.8 This may need to be adjusted if the cards are still too wide
-                      sizedBoxHeight: fontSize *
-                          3.0, // 9.1 This may need to be adjusted if the cards are still too tall
-                      fontSize: fontSize *
-                          2.5, // Ensure this is appropriate for the size of the cards
-                      isVertical: true,
-                    );
-                  },
-                  childCount: wordsStatus.length,
-                ),
+        title: const Text("00 | 學注音"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () => Navigator.of(context).pushNamed('/mainPage'),
+          )
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(14, 8, 14, 16),
+            sliver: SliverGrid(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 125.0,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 113 / 166,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return WordCard(
+                    disable: false,
+                    isBpmf: true,
+                    unitId: 0,
+                    unitTitle: "學注音",
+                    wordsStatus: wordsStatus,
+                    wordsPhrase: wordsPhrase,
+                    wordIndex: index,
+                    sizedBoxWidth: 30,
+                    sizedBoxHeight: 155,
+                    fontSize: 48,
+                    isVertical: true,
+                  );
+                },
+                childCount: wordsStatus.length,
               ),
             ),
-            SliverPadding(
-              padding: const EdgeInsets.fromLTRB(46, 16, 46, 16),
-              sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate(
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(46, 16, 46, 16),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
                 (BuildContext context, int idx) {
                   return InkWell(
                     onTap: () => Navigator.of(context).pushNamed('/bopomoQuiz'),
                     child: Container(
-                      width: fontSize * 17.5,
-                      height: fontSize * 4.0,
+                      width: 297,
+                      height: 80,
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
                         borderRadius:
-                            const BorderRadius.all(Radius.circular(14)),
+                          const BorderRadius.all(Radius.circular(14)),
                         color: "#F5F5DC".toColor(),
                       ),
                       child: Text(
                         "拼拼看",
                         style: TextStyle(
-                          fontSize: fontSize * 2.0,
+                          fontSize: 32,
                           color: "#28231D".toColor(),
                         ),
                         textAlign: TextAlign.center,
@@ -99,8 +86,8 @@ class BopomosView extends StatelessWidget {
                 },
                 childCount: 1,
               )),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ));
   }
 }
