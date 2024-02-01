1963d3178a7b6faaa1fd9d94a2427d5495d78439
Shinjou <shinjou.fang@gmail.com>
2024-02-01 16:25:20 +0800

    開發者

--- lib/views/acknowledge.dart
+++ lib/views/acknowledge.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ltrc/views/view_utils.dart';
 
-class Resource {
+class Resource{
   final String resource;
   final String description;
 
@@ -9,18 +8,30 @@ class Resource {
 }
 
 final List<Resource> resourceList = [
-  Resource(resource: "各年級生字表", description: "由中研院語言學研究所李佳穎老師提供，用於所有功能。"),
   Resource(
-      resource: "注音字卡",
-      description: "由郭俊成老師提供,來自fb粉絲專頁「麻辣飛天郭」,用於注音「看一看」、「用一用」及「拼拼看」。"),
+    resource: "各年級生字表",
+    description: "由中研院語言學研究所李佳穎老師提供，用於所有功能。"
+  ),
   Resource(
-      resource: "字型演變", description: "來自中央研究院歷史語言學研究所與資訊科學研究所開發之「小學堂」字型演變資料庫。"),
-  Resource(resource: "筆順動畫", description: "來自文鼎科技開發股份有限公司提供之開源漢字、注音筆順素材與技術支持。"),
-  Resource(resource: "諮詢顧問", description: "鄭漢文校長與陳素慧老師擔任諮詢顧問，分享現場教師觀點。"),
+    resource: "注音字卡", 
+    description: "由郭俊成老師提供,來自fb粉絲專頁「麻辣飛天郭」,用於注音「看一看」、「用一用」及「拼拼看」。"
+  ),
   Resource(
-      resource: "APP開發", description: "林羿成、蔡伊甯、李昊翰、方新舟合力開發此軟體。"),
+    resource: "字型演變",
+    description: "來自中央研究院歷史語言學研究所與資訊科學研究所開發之「小學堂」字型演變資料庫。"
+  ),
   Resource(
-      resource: "版權所有©2024誠致教育基金會", description: '')    
+    resource: "筆順動畫",
+    description: "來自文鼎科技開發股份有限公司提供之開源漢字、注音筆順素材與技術支持。"
+  ),
+  Resource(
+    resource: "諮詢顧問",
+    description: "鄭漢文校長與陳素慧老師擔任諮詢顧問，提供寶貴現場教學經驗。"
+  ),
+  Resource(
+    resource: "APP開發",
+    description: "林羿成、蔡伊甯、李昊翰、方新舟合力開發此軟體。版權歸誠致教育基金會所有。"
+  )
 ];
 
 class AcknowledgeView extends StatelessWidget {
@@ -28,43 +39,47 @@ class AcknowledgeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double fontSize =
-        getFontSize(context, 16); // 16 is the base font size for 360dp width
-
     int count = 0;
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.chevron_left, size: fontSize * 1.0),
-            onPressed: () => Navigator.pop(context),
-          ),
-          title: Text("授權與致謝",
-              style: TextStyle(
-                fontSize: fontSize * 1.0,
-              )),
-        ),
-        body: CustomScrollView(slivers: [
-          SliverPadding(
-              padding: const EdgeInsets.fromLTRB(23, 14, 23, 20),
-              sliver: SliverToBoxAdapter(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("",           //"版權所有©2024誠致教育基金會" moved to the bottom
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                              fontSize: fontSize * 0.3, fontWeight: FontWeight.normal)),
-                      ...resourceList.map((e) {
-                        count++;
-                        return Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 7, 0, 7),
-                            child: Text(
-                                "$count. ${e.resource}\n ${e.description}",
-                                style: TextStyle(fontSize: fontSize)));
-                      }).toList()
-                    ]),
-              ))
-        ]));
+      appBar: AppBar(
+        leading: IconButton(icon: const Icon(Icons.chevron_left), onPressed: () => Navigator.pop(context),),
+        title: const Text("致謝"),
+      ),
+      body: CustomScrollView(
+        slivers: [
+            SliverPadding(
+            padding: const EdgeInsets.fromLTRB(23, 14, 23, 20),
+            sliver: SliverToBoxAdapter( 
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "圖片資源來源：",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  ...resourceList.map((e){
+                    count ++;
+                    return Padding( 
+                      padding: const EdgeInsets.fromLTRB(0, 7, 0, 7),
+                      child:Text(
+                        "$count. ${e.resource}\n ${e.description}",
+                        style: const TextStyle(
+                          fontSize: 16
+                        )
+                      )
+                    );
+                  })
+                ]
+              ),
+            )
+          )
+        ]
+      )
+    );
   }
 }
