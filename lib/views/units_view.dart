986e4d62adeb2a2a023f5e61e57aa61f1a825e99
Shinjou <shinjou.fang@gmail.com>
2024-02-03 19:55:01 +0800

    Perspective UI

--- lib/views/units_view.dart
+++ lib/views/units_view.dart
@@ -8,8 +8,9 @@ import 'package:ltrc/data/providers/phrase_provider.dart';
 import 'package:ltrc/data/providers/word_phrase_provider.dart';
 import 'package:ltrc/data/providers/word_provider.dart';
 import 'package:ltrc/data/providers/word_status_provider.dart';
-import 'package:ltrc/extensions.dart';
+// import 'package:ltrc/extensions.dart';
 import 'package:ltrc/providers.dart';
+import 'package:ltrc/views/view_utils.dart';
 
 import '../contants/arabic_numerals_to_chinese.dart';
 import '../data/models/unit_model.dart';
@@ -35,12 +36,10 @@ class UnitsViewState extends ConsumerState<UnitsView> {
 
   @override
   Widget build(BuildContext context) {
-    
-    dynamic obj = ModalRoute.of(context)!.settings.arguments;
+    double fontSize = getFontSize(context, 16); // Adjust this function to be more responsive
 
-    setState(() {
-      units = obj["units"];
-    });
+    dynamic obj = ModalRoute.of(context)!.settings.arguments;
+    List<Unit> units = obj["units"]; // Assuming 'units' is a list of 'Unit'
 
     Future<List<Map>> getWordsPhrase(List<WordStatus> wordsStatus) async {
       List<Map> wordsPhrase = [];
@@ -81,26 +80,29 @@ class UnitsViewState extends ConsumerState<UnitsView> {
     
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(icon: const Icon(Icons.chevron_left), onPressed: () => Navigator.pop(context),),
-        title: const Text("課程單元"),
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left, size: fontSize * 1.5), 
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text("課程單元", style: TextStyle(fontSize: fontSize * 1.5)),
       ),
-
       body: CustomScrollView(
-          slivers: [
-             SliverPadding(
-              padding: const EdgeInsets.fromLTRB(23, 14, 23, 20),
-                sliver: SliverGrid(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 180.0,
-                  mainAxisSpacing: 13.0,
-                  crossAxisSpacing: 13.0,
-                  childAspectRatio: 2,
-                ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return index == 0 ?
-                    InkWell( 
-                      onTap: () async {
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(23, 14, 23, 20),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // for 2 columns, adjust according to your design
+                childAspectRatio: 6 / 2, // was 3 / 2
+                crossAxisSpacing: fontSize * 0.66, // was 10,
+                mainAxisSpacing: fontSize * 0.66 // was 10,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  String text = index == 0 ? "學注音" : "最愛❤️";
+                  return InkWell(
+                    onTap: () async {
+                      if (index == 0 ) {  // 學注音
                         List<String> bopomos = List.from(initials)..addAll(prenuclear)..addAll(finals);
                         await WordStatusProvider.addWordsStatus(
                           statuses: bopomos.map((word) => 
@@ -119,7 +121,7 @@ class UnitsViewState extends ConsumerState<UnitsView> {
                           words: bopomos, 
                         );
                         List<Map> bpmfWordsPhrase = await getWordsPhrase(wordsStatus);
-
+                        if (!mounted) return;
                         Navigator.of(context).pushNamed(
                           '/bopomos', 
                           arguments: {
@@ -127,25 +129,8 @@ class UnitsViewState extends ConsumerState<UnitsView> {
                             'wordsPhrase' : bpmfWordsPhrase
                           }
                         );
-                      },
-                      child: Container(
-                        width: 140,
-                        height: 80,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(14)),
-                          color: "#013E6D".toColor(),
-                        ),
-                        child: const Text(
-                          "學注音", 
-                          style: TextStyle(
-                            fontSize: 24,
-                          ), 
-                          textAlign: TextAlign.center,),
-                      ),
-                    ) :
-                    InkWell( 
-                      onTap: () async {
+                      }
+                      else {
                         List<WordStatus> likedWords = await WordStatusProvider.getLikedWordsStatus(
                           account: ref.watch(accountProvider), 
                         );
@@ -167,113 +152,119 @@ class UnitsViewState extends ConsumerState<UnitsView> {
                             'newWordsPhrase' : likedWordsPhrase
                           }
                         );
-                      },
-                      child: Container(
-                        width: 140,
-                        height: 80,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(14)),
-                          color: "#013E6D".toColor(),
-                        ),
-                        child: const Text(
-                          "❤️最愛", 
-                          style: TextStyle(
-                            fontSize: 24,
-                          ), 
-                          textAlign: TextAlign.center,
+                      }
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(14)),
+                        color: Color(0xFF013E6D),
+                      ),
+                      child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Text(
+                            text,
+                            style: TextStyle(fontSize: fontSize),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                       ),
-                    );
-                  },
-                  childCount: 2,
-                ),
+                    ),
+                  );
+                },
+                childCount: 2,
               ),
             ),
-            SliverPadding(
-              padding: const EdgeInsets.fromLTRB(23, 14, 23, 20),
-                sliver: SliverGrid(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 180.0,
-                  mainAxisSpacing: 13.0,
-                  crossAxisSpacing: 13.0,
-                  childAspectRatio: 1.05,
-                ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    String? classNum = numeralToChinese[index+1];
-                    return InkWell( 
-                      onTap: () async {
-                        Unit unit = units[index];
-                        await WordStatusProvider.addWordsStatus(
-                          statuses: unit.newWords.map((word) => 
-                            WordStatus(
-                              id: -1, 
-                              userAccount: ref.read(accountProvider.notifier).state , 
-                              word: word, 
-                              learned: false, 
-                              liked: false
-                            )
-                          ).toList()
-                        );
-                        unit.newWords.removeWhere((item) => unit.extraWords.contains(item));
-                        
-                        List<WordStatus> newWordsStatus = await WordStatusProvider.getWordsStatus(
-                          account: ref.read(accountProvider.notifier).state,
-                          words: unit.newWords, 
-                        );
-                        List<WordStatus> extraWordsStatus = await WordStatusProvider.getWordsStatus(
-                          account: ref.read(accountProvider.notifier).state,
-                          words: unit.extraWords, 
-                        );
-                        List<Map> newWordsPhrase = await getWordsPhrase(newWordsStatus);
-                        List<Map> extraWordsPhrase = await getWordsPhrase(extraWordsStatus);
+          ),
 
-                        Navigator.of(context).pushNamed(
-                          '/words', 
-                          arguments: {
-                            'unit' : unit,
-                            'newWordsStatus' : newWordsStatus,
-                            'extraWordsStatus' : extraWordsStatus,
-                            'newWordsPhrase' : newWordsPhrase,
-                            'extraWordsPhrase' : extraWordsPhrase
-                          }
-                        );
-                      },
-                      child: Container(
-                        width: 140,
-                        height: 140,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(14)),
-                          color: "#013E6D".toColor(),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "第$classNum課", 
-                              style: const TextStyle(
-                                fontSize: 16,
-                              )
-                            ),
-                            Text(
-                              units[index].unitTitle, 
-                              style: const TextStyle(
-                                fontSize: 24,
-                              ),
-                              textAlign: TextAlign.center,
-                            )
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                  childCount: units.length,
-                ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(23, 14, 23, 20),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // for three columns, adjust according to your design
+                childAspectRatio: 6 / 3, // was 3 / 2
+                crossAxisSpacing: fontSize * 0.66, // was 10,
+                mainAxisSpacing: fontSize * 0.66, // was 10,
               ),
-            )
-          ],
-        ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  // String? classNum = numeralToChinese[index + 1];
+                  return InkWell(
+                    onTap: () async {
+                      Unit unit = units[index];
+                      await WordStatusProvider.addWordsStatus(
+                        statuses: unit.newWords.map((word) => 
+                          WordStatus(
+                            id: -1, 
+                            userAccount: ref.read(accountProvider.notifier).state , 
+                            word: word, 
+                            learned: false, 
+                            liked: false
+                          )
+                        ).toList()
+                      );
+                      unit.newWords.removeWhere((item) => unit.extraWords.contains(item));
+                      
+                      List<WordStatus> newWordsStatus = await WordStatusProvider.getWordsStatus(
+                        account: ref.read(accountProvider.notifier).state,
+                        words: unit.newWords, 
+                      );
+                      List<WordStatus> extraWordsStatus = await WordStatusProvider.getWordsStatus(
+                        account: ref.read(accountProvider.notifier).state,
+                        words: unit.extraWords, 
+                      );
+                      List<Map> newWordsPhrase = await getWordsPhrase(newWordsStatus);
+                      List<Map> extraWordsPhrase = await getWordsPhrase(extraWordsStatus);
+                      if (!mounted) return;
+                      Navigator.of(context).pushNamed(
+                        '/words', 
+                        arguments: {
+                          'unit' : unit,
+                          'newWordsStatus' : newWordsStatus,
+                          'extraWordsStatus' : extraWordsStatus,
+                          'newWordsPhrase' : newWordsPhrase,
+                          'extraWordsPhrase' : extraWordsPhrase
+                        }
+                      );
+                    },            
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(14)),
+                        color: Color(0xFF013E6D),
+                      ),
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(2.0), // was 8.0
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "第${numeralToChinese[index + 1]}課",
+                            style: TextStyle(
+                              fontSize: fontSize, // Choose a font size that fits all boxes
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            units[index].unitTitle,
+                            style: TextStyle(fontSize: fontSize), // Use the same font size for consistency
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                childCount: units.length,
+              ),
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
