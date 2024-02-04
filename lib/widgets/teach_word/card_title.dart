25936f1d9ea2a912bdd5f57945f0ec919c57e83c
Shinjou <shinjou.fang@gmail.com>
2024-02-04 10:50:08 +0800

    UI changes

--- lib/widgets/teach_word/card_title.dart
+++ lib/widgets/teach_word/card_title.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ltrc/views/view_utils.dart';
 
 class TeachWordCardTitle extends StatelessWidget {
   final Color iconsColor;
@@ -8,25 +9,25 @@ class TeachWordCardTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double deviceWidth = MediaQuery.of(context).size.width;
-    
+    double fontSize =
+        getFontSize(context, 16); // 16 is the base font size for 360dp width
+
     return Container(
-      width: MediaQuery.of(context).size.width * 0.44,
-      height: 38,
-      alignment: Alignment.center,
-      margin: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(38 / 2)),
-        color: iconsColor,
-      ),
-      child: Text(
-        sectionName,
-        style: TextStyle(
-          fontSize: deviceWidth * 24/360,
-          fontWeight: FontWeight.w900,
-          color: Colors.black,
+        width: fontSize * 8.0, // for 3 1.5 fontSize characters
+        height: fontSize * 2.0, // word fontSize = 1.8
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(38 / 2)),
+          color: iconsColor,
         ),
-      )
-    );
+        child: Text(
+          sectionName,
+          style: TextStyle(
+            fontSize: fontSize * 1.3, // was 24/360
+            fontWeight: FontWeight.w900,
+            color: Colors.black,
+          ),
+        ));
   }
 }
