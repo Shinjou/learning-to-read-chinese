930b475d7e1506b81c096d382806da9ca43b83dd
Shinjou <shinjou.fang@gmail.com>
2024-02-28 21:02:55 +0800

    add code to handle DB upgrade

--- lib/data/providers/user_provider.dart
+++ lib/data/providers/user_provider.dart
@@ -24,8 +24,10 @@ class UserProvider {
   static const String databaseSemester = 'semester';
   static const String databasePublisher = 'publisher';
 
-  static const int _dbVersion = 3;
   static const String _dbName = 'users.sqlite';
+    static const int _dbVersion = 4; // 2/25/2024
+  static const String _wordStatusTable = 'wordStatus';
+  // static const String _usersTable = 'users'; 等以後需要時，再加入
 
   static Future<Database> get database async {
     _database ??= await _initDatabase();
@@ -34,49 +36,103 @@ class UserProvider {
 
   static Future<Database> getDBConnect() async {
     if (!dbExists){
-      _database ??= await _initDatabase();
+      _database = await _initDatabase();
     }
     return _database!;
   }  
 
-  static Future<Database> _initDatabase() async {
+  static Future<Database?> _initDatabase() async {
     try {
       String dbPath = join(await getDatabasesPath(), _dbName);
-      debugPrint('Opening users database at $dbPath');
+      debugPrint('Path to $_dbName: $dbPath');
 
-      // Check if the database exists
-      dbExists = await databaseExists(dbPath);
-
+      bool dbExists = await databaseExists(dbPath);
       if (!dbExists) {
-        // Copy from assets if the database doesn't exist
-        debugPrint('Copying $_dbName from assets/data_files/...');
-        ByteData data = await rootBundle.load(join('assets/data_files/', _dbName));
-        List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-        await File(dbPath).writeAsBytes(bytes, flush: true);
-      } else {
-        debugPrint('Opening existing $_dbName...');
-      }
+        await _copyDbFromAssets(dbPath);
+        debugPrint('$_dbName copied from assets.');
+      } 
 
       // Open the database
-      return await openDatabase(
-        dbPath,
-        version: _dbVersion,
-        onUpgrade: _onUpgrade,
-      );
+      _database = await openDatabase(dbPath);
+
+      // Checking and potentially upgrading the database version
+      int currentVersion = await _database!.getVersion();
+
+      if (currentVersion < _dbVersion) {
+        debugPrint('Upgrading $_dbName from version $currentVersion to $_dbVersion ...');
+        // Ver 4，新增“鸚”一字
+        await deleteWord(db: _database, userAccount: 'tester', word: '鸚');
+        await addWord(db: _database, userAccount: 'tester', word: '鸚', learned: 1, liked: 1);
+        await _database!.setVersion(_dbVersion);  
+        debugPrint('Upgrade $_dbName successfully...');
+      } else {
+        debugPrint('Database $_dbName opened successfully...');
+      }
+
+      return _database!;
     } catch (e) {
-      debugPrint('Error initializing the database: $e');
+      debugPrint('Error initializing $_dbName: $e');
+      rethrow; // Consider handling this more gracefully
+    }
+  }
+
+  static Future<void> _copyDbFromAssets(String dbPath) async {
+    try {
+      debugPrint('Copying $_dbName from assets/data_files/...');
+      ByteData data = await rootBundle.load(join('assets/data_files/', _dbName));
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(dbPath).writeAsBytes(bytes, flush: true);
+      debugPrint('Copy of $_dbName to $dbPath completed successfully.');
+    } catch (e) {
+      debugPrint('Failed to copy $_dbName from assets to $dbPath. Error: $e');
+      // Depending on your use case, you might want to rethrow the error,
+      // return a failure status, or attempt a recovery operation here.
+      // For example, to rethrow the error, uncomment the following line:
       rethrow;
     }
   }
 
-  static Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
-    // Handle database upgrades
-    if (oldVersion < newVersion) {
-      // Example: if (oldVersion < 2) { await db.execute("ALTER TABLE ..."); }
-      // Add other migration logic as needed
+  static Future<void> addWord({
+    required Database? db,
+    required String userAccount,
+    required String word,
+    required int learned,
+    required int liked,
+  }) async {
+    try {
+      await db?.insert(
+        _wordStatusTable, // Assuming 'wordStatus' is the correct table name
+        {
+          'userAccount': userAccount,
+          'word': word,
+          'learned': learned,
+          'liked': liked,
+        },
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+      debugPrint("Word $word added successfully");
+    } catch (e) {
+      debugPrint('An error occurred while inserting the word: $e');
     }
-  }  
-  
+  }
+
+  static Future<void> deleteWord({
+    required Database? db,
+    required String userAccount,
+    required String word,
+  }) async {
+    try {
+      await db?.delete(
+        _wordStatusTable,
+        where: 'userAccount = ? AND word = ?', // Define the WHERE clause
+        whereArgs: [userAccount, word], // Provide the values for the WHERE clause
+      );
+      debugPrint("Word $word deleted successfully");
+    } catch (e) {
+      debugPrint('An error occurred while deleting the word: $e'); // Corrected error message
+    }
+  }
+
   static Future<void> addUser({required User user}) async {
     final Database db = await getDBConnect();
     await db.insert(
@@ -156,7 +212,6 @@ class UserProvider {
       // Consider using more sophisticated error reporting if available in your app
     }
   }
-
 
   static Future<void> closeDb() async {
     if (_database != null) {
