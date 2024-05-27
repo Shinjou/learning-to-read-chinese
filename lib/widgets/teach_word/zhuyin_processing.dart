f8ff82b17da8df44e21da2b98bd65dcb642c0fe7
Shinjou <shinjou.fang@gmail.com>
2024-05-27 14:26:06 +0800

    處理多音字的 UI

---
+++ lib/widgets/teach_word/zhuyin_processing.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ltrc/providers.dart';
+import 'package:ltrc/views/polyphonic_processor.dart';
+// import 'package:ltrc/views/view_utils.dart';
+import 'package:tuple/tuple.dart';
+
+class ZhuyinProcessing extends ConsumerWidget {
+  final String text;
+  final double fontSize;
+  final Color color;
+  final bool highlightOn;
+
+  const ZhuyinProcessing({
+    super.key,
+    required this.text,
+    required this.fontSize,
+    required this.color,
+    required this.highlightOn,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int grade = ref.watch(gradeProvider);
+
+    if (grade < 5) {
+      return FutureBuilder<Tuple2<List<TextSpan>, String>>(
+        // future: _processText(fontSize, text),
+        future: PolyphonicProcessor.instance.process(text, fontSize, color, highlightOn),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+            return RichText(
+              text: TextSpan(children: snapshot.data!.item1),
+              // style: TextStyle(
+              //   fontSize: fontSize,
+              //   color: color,
+              // ),
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error loading polyphonic data',
+              style: TextStyle(
+                fontSize: fontSize,
+                color: color,
+              ),
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      );
+    } else {
+      return Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+          color: color,
+        ),
+      );
+    }
+  }
+}
