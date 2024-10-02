aa024c5c7fb0b2d60eb497024c5d42e46118f847
Shinjou <shinjou.fang@gmail.com>
2024-10-02 16:55:03 +0800

    clean up file

--- lib/views/view_utils.dart
+++ lib/views/view_utils.dart
@@ -1,4 +1,5 @@
 // view_utils.dart
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -88,66 +89,6 @@ class ScreenInfoNotifier extends StateNotifier<ScreenInfo> {
   }  
 }
 
-/*
-class ScreenInfo {
-  final double screenHeight;
-  final double screenWidth;
-  final double fontSize;
-  final Orientation orientation;
-  final bool isTablet;
-
-  ScreenInfo({
-    required this.screenHeight,
-    required this.screenWidth,
-    required this.fontSize,
-    required this.orientation,
-    required this.isTablet,
-  });
-}
-
-class ScreenInfoNotifier extends StateNotifier<ScreenInfo> {
-  ScreenInfoNotifier() : super(ScreenInfo(
-    screenHeight: 0,
-    screenWidth: 0,
-    fontSize: 0,
-    orientation: Orientation.portrait,
-    isTablet: false,
-  ));
-
-  void init(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    double screenHeight = mediaQuery.size.height;
-    double screenWidth = mediaQuery.size.width;
-    double shortestSide = mediaQuery.size.shortestSide;
-    Orientation orientation = mediaQuery.orientation;
-    bool isTablet = shortestSide > 600;
-
-    double baseScreenWidth = isTablet ? 600 : 360;
-    double baseFontSize = isTablet ? 24.0 : 15.0;
-
-    if (orientation == Orientation.landscape) {
-      screenHeight = mediaQuery.size.shortestSide;
-      screenWidth = screenHeight * 3 / 4;
-    }
-
-    double fontSize = (baseFontSize * screenWidth / baseScreenWidth).roundToDouble();
-
-    final newState = ScreenInfo(
-      screenHeight: screenHeight,
-      screenWidth: screenWidth,
-      fontSize: fontSize,
-      orientation: orientation,
-      isTablet: isTablet,
-    );
-
-    if (newState != state) {
-      state = newState;
-      debugPrint('ScreenInfoNotifier updated: H: $screenHeight, W: $screenWidth, F: $fontSize, $orientation, T: $isTablet');
-    }
-  }
-}
-*/
-
 /* Usage of navigateWithProvider
 1. No arguments to pass: mainPage needs to be defined in AppRoutes
 onPressed: () => navigateWithProvider(context, '/mainPage', ref),
@@ -167,22 +108,27 @@ void navigateWithProvider(
     {Map<String, dynamic>? arguments}) {
    
   // Extract the caller method using the stack_trace package
-  String callerInfo = "";
-  try {
-    // Capture the current stack trace
-    final stackTrace = StackTrace.current;
-    // Use Trace from stack_trace package to parse it
-    final trace = Trace.from(stackTrace);
+  // Only enable the following code in development mode
 
-    // The second frame in the trace is likely the caller method
-    final frame = trace.frames[1]; // Adjust frame index as needed
-    callerInfo = '${frame.member} in ${frame.library}';
-  } catch (e) {
-    callerInfo = "Caller unknown";
+  if (kDebugMode) {
+    String callerInfo = "";
+    try {
+      // Capture the current stack trace
+      final stackTrace = StackTrace.current;
+      // Use Trace from stack_trace package to parse it
+      final trace = Trace.from(stackTrace);
+
+      // The second frame in the trace is likely the caller method
+      final frame = trace.frames[1]; // Adjust frame index as needed
+      callerInfo = '${frame.member} in ${frame.library}';
+    } catch (e) {
+      callerInfo = "Caller unknown";
+    }
+    debugPrint("navigateWithProvider called by $callerInfo, to $routeName");
   }
+
   final screenInfo = ref.read(screenInfoProvider);
-  // Print the caller method and the route it will navigate to
-  debugPrint("navigateWithProvider called by $callerInfo, to $routeName with screenInfo: H: ${screenInfo.screenHeight}, W: ${screenInfo.screenWidth}, F: ${screenInfo.fontSize}");
+  debugPrint("navigateWithProvider screenInfo: H: ${screenInfo.screenHeight}, W: ${screenInfo.screenWidth}, F: ${screenInfo.fontSize}");
 
   final routes = AppRoutes.define();
 
