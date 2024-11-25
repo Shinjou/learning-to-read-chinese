1ed0301d1058a53750999c42a3d54b80f7f928ef
Shinjou <shinjou.fang@gmail.com>
2024-11-25 22:09:25 +0800

    Moved teach_word_view.dart to lib/teach_word/presentatin/.

--- lib/contants/routes.dart
+++ lib/contants/routes.dart
@@ -17,7 +17,7 @@ import 'package:ltrc/views/safety_hint_verify_view.dart';
 import 'package:ltrc/views/setting_view.dart';
 import 'package:ltrc/views/sw_version_view.dart';
 // import 'package:ltrc/views/teach_word_view.dart';
-import 'package:ltrc/teach_word/presentation/teach_word_view_testing.dart';
+import 'package:ltrc/teach_word/presentation/teach_word_view.dart';
 import 'package:ltrc/views/units_view.dart';
 
 import 'package:ltrc/views/words_view.dart';
