5c7408a7d552505cf3f378852ee7f7f8d27b2e40
Shinjou <shinjou.fang@gmail.com>
2025-01-15 07:52:07 +0800

    Changes due to Tool changes (Flutter, Javo, Android Studio, etc.)

--- android/app/src/main/kotlin/org/chengzhiedu/ltrc/ltrc/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package org.chengzhiedu.ltrc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,8 @@ import package_info_plus
 import path_provider_foundation
 import record_darwin
 import shared_preferences_foundation
-import speech_to_text
-import sqflite_darwin
+import speech_to_text_macos
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -21,6 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
